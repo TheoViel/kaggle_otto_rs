9e02ecb7049906ff25fa3a6dacfa21e7d7e4c55a
theoviel <theo.viel.enpc@gmail.com>
2022-11-03 15:47:25 +0000

    dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,43 +1,72 @@
 import torch
 import numpy as np
+from scipy.stats import poisson
 from torch.utils.data import Dataset
 
+from params import N_IDS
 
-class PatientNoteDataset(Dataset):
-    def __init__(self, df, tokenizer, max_len, soft_labels=False):
+class OttoTrainDataset(Dataset):
+    def __init__(self, df, max_len=100, max_trunc=20, train=True, test=False):
         self.df = df
+        self.test = test
+        self.train = train
         self.max_len = max_len
-        self.tokenizer = tokenizer
+        self.max_trunc = max_trunc
+        self.paths = df['path'].values
+        
+        self.mu = 25
+        self.ps = poisson.pmf(np.arange(self.mu, 1000), mu=self.mu)
+        self.ps[0] *= 1.5  # More 1s
+        self.ps[9:] += 0.01  # Higher tail
 
-        self.texts = df['clean_text'].values
-#         self.feature_text = df['feature_text'].values
+    def pad(self, x):
+        length = x.shape[0]
+        if length > self.max_len:
+            return x[: self.max_len]
+        else:
+            padded = np.zeros([self.max_len] + list(x.shape[1:]))
+            padded[:length] = x
+            return padded
+        
+    def truncate(self, x):
+        if len(x) <= self.max_trunc:
+            range_ = np.arange(1, len(x))
+        elif len(x) > self.max_len:
+            range_ = np.arange(self.max_len - self.max_trunc, self.max_len)
+        else:
+            range_ = np.arange(len(x) - self.max_trunc, len(x))
+        
+        ps = self.ps[:len(range_)].copy()
+        ps /= ps.sum()
 
-        self.targets = df['target'].values
+        if not self.train:  # deterministic
+            np.random.seed(2020)
+        trunc = np.random.choice(range_, p=ps)
+
+        return x[:trunc], x[trunc:]
+        
+    @staticmethod
+    def get_target(x):
+        y = np.zeros((N_IDS, NUM_CLASSES), dtype=np.uint8)
+        y[x[:, 0], x[:, 2]] = 1
+        return y.copy()
 
     def __getitem__(self, idx):
-        text = self.texts[idx]
-#         feature_text = self.feature_text[idx]
+        x = np.load(self.paths[idx])
 
-        # Tokenize
-        encoding = self.tokenizer(
-            text,
-#             text,
-            return_token_type_ids=True,
-            return_offsets_mapping=False,
-            return_attention_mask=False,
-            truncation="only_first",
-            max_length=self.max_len,
-            padding='max_length',
-        )
-        # Targets
-        token_target = self.targets[idx]
+        if not self.test:
+            x, x_target = self.truncate(x)
+            y = self.get_target(x_target)
+
+        x = self.pad(x)
 
         return {
-            "ids": torch.tensor(encoding["input_ids"], dtype=torch.long),
-            "token_type_ids": torch.tensor(encoding["token_type_ids"], dtype=torch.long),
-            "target": torch.tensor(token_target, dtype=torch.float),
-            "text": text,
+            "ids": torch.tensor(x[:, 0], dtype=torch.long),
+            "ts": torch.tensor(x[:, 1], dtype=torch.long),
+            "token_type_ids": torch.tensor(x[:, 2], dtype=torch.long),
+            "x_target": torch.tensor(x_target),
+            "target": torch.from_numpy(y),
         }
 
     def __len__(self):
-        return len(self.texts)
+        return len(self.paths)
--- src/params.py
+++ src/params.py
@@ -4,13 +4,14 @@ K = 5
 DATA_PATH = "../input/"
 OUT_PATH = "../output/"
 
-NUM_CLASSES = 1
+CLASSES = ['clicks', 'carts', 'orders']
+NUM_CLASSES = 3
 
 LOG_PATH = "/workspace/logs/"
 
 NUM_WORKERS = 4
 
-N_IDS = 1855600 + 1
+N_IDS = 1855605
 
 TRAIN_DATA_PATH = DATA_PATH + "processed/"
 TEST_DATA_PATH = DATA_PATH + "processed_test/"
