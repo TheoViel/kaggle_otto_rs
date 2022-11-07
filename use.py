b7b4bbcd494fbe60c0ef7768712b7bfa57974525
theoviel <theo.viel.enpc@gmail.com>
2022-11-07 11:53:14 +0000

    pq

---
+++ use.py
@@ -0,0 +1,12 @@
+import time
+import torch
+from tqdm import tqdm
+
+x = torch.rand((512, 1024, 1024)).cuda()
+y = torch.rand((512, 1024, 1024)).cuda()
+
+minutes = 60
+
+for _ in tqdm(range(100 * 60 * minutes)):
+    time.sleep(0.01)
+    y = (x * y)
